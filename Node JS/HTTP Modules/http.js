ec85b9ff29fda4dd65f13d04425a25378d491362
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2024-03-16 11:17:01 +0530

    Update http.js

--- Node JS/HTTP Modules/http.js
+++ Node JS/HTTP Modules/http.js
@@ -9,7 +9,7 @@ const port = 4000;
 //create Server using createServer() method
 http.createServer((req, res) => {
     res.write("Hello Server Created Using http Module");
-    res.end();
+    res.end(); //must end response
 }).listen(port);
 
 console.log(`Server Started on port ${port}`)
