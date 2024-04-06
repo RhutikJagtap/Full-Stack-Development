37bc244c27b0316b7a8e9e5106530e379d8eee1b
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2024-04-07 00:36:31 +0530

    Add files via upload

---
+++ JavaScript/variable.js
@@ -0,0 +1,19 @@
+const RollNo = 15452
+//we cannot change constant variable
+//RollNo = 715453
+console.log("RollNo=" + RollNo)
+
+//let varible we can not  declare multiple times but we can assign value
+let email = "rhutikjagtap68@gmail.com"
+console.log("Email Id=" + email)
+
+// var variable we can declare multiple times
+var contact = "7385893771"
+console.log("contact=" + contact)
+
+city = "pune"
+console.log("City=" + city)
+
+console.log("==============")
+
+console.table([RollNo, email, contact, city])
