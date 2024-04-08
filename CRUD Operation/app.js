8bfcc8e6efcbbcac73f00cf6d9adb491da5f1c7e
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2024-04-08 20:32:31 +0530

    Create app.js

---
+++ CRUD Operation/app.js
@@ -0,0 +1,24 @@
+const Connection = require("./connection")
+
+const InsertDocumentt = require("./Insert")
+
+const UpdateBook = require("./Update")
+
+const DeleteDocument = require("./Delete")
+
+const AllDocuments = require('./Fetch')
+
+console.log(Connection);
+
+//calling
+//InsertDocumentt();
+
+
+//calling Update Book
+// UpdateBook()
+
+// Calling Delete Document
+// DeleteDocument()
+
+//calling for fetch document
+AllDocuments();
